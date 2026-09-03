c35a775b282a5ca1e5756a2f66a7bdc68f260963
anttiak <antti.ak@gmail.com>
2026-09-03 14:22:08 +0300

    Add LoFeline cask

---
+++ Casks/lofeline.rb
@@ -0,0 +1,15 @@
+cask "lofeline" do
+  version "1.0.0"
+  sha256 "3358e730dc3565f9ad5007bf96d6baeeb4745511494288c065033d387bb8a31d"
+
+  url "https://github.com/anttiak/lofeline-macos/releases/download/v#{version}/LoFeline-#{version}.zip"
+  name "LoFeline"
+  desc "Tiny pixel cat that naps in your menu bar and plays lofi beats"
+  homepage "https://github.com/anttiak/lofeline-macos"
+
+  depends_on macos: ">= :sonoma"
+
+  app "LoFeline.app"
+
+  zap trash: "~/Library/Preferences/io.github.anttiak.LoFeline.plist"
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# homebrew-tap
+
+Homebrew tap for [anttiak](https://github.com/anttiak)'s apps.
+
+## LoFeline
+
+```sh
+brew install --cask --no-quarantine anttiak/tap/lofeline
+```
+
+`--no-quarantine` is needed because the app isn't notarized.
